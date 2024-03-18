1a90c4dc0aa4c4079a905abde32df15cb2d58eb9
Kage Jinsei <dadinho_123@outlook.com>
2024-03-18 05:48:47 -0300

    Substituindo o fmt.Scan pelo bufio.NewScanner para receber os inputs fazendo algumas otimizações

--- characterCreation.go
+++ characterCreation.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 var raca string
 var classe string
@@ -8,120 +12,128 @@ var local string
 var nome string
 
 func main() {
-  fmt.Println("RPG - IMAGINA UM NOME FODA AQUI!\n")
-  
-  borda()
-  racas()
-  borda()
-  classes()
-  borda()
-  locais()
-  borda()
+	fmt.Println("RPG - IMAGINA UM NOME LEGAL AQUI!")
 
-  fmt.Print("\nInsira o nome da sua personagem: => ")
-  fmt.Scan(&nome)
-  fmt.Print("\n")
-  
-  borda()
-  ficha()
-  borda()
+	borda()
+	racas()
+	borda()
+	classes()
+	borda()
+	locais()
+	borda()
+	ficha()
+	borda()
 }
 
 func racas() {
-  fmt.Println("SELECIONE A SUA RAÇA:")
-  fmt.Print("|1| Humano\n" +
-            "|2| Elfo\n" +
-            "|3| Fada\n" +
-            "|4| Besta\n" +
-            "|5| Vagante\n")
-  fmt.Print("Qual raça deseja?: => ") 
-  fmt.Scan(&raca)
+	fmt.Println("SELECIONE A SUA RAÇA:")
+	fmt.Print("|1| Humano\n" +
+		"|2| Elfo\n" +
+		"|3| Fada\n" +
+		"|4| Besta\n" +
+		"|5| Vagante\n")
+	fmt.Print("Qual raça deseja?: => ")
 
-  switch {
-  case raca == "1":
-    raca = "Humano"
-  
-  case raca == "2":
-    raca = "Elfo"
+	scanner := bufio.NewScanner(os.Stdin)
 
-  case raca == "3":
-    raca = "Fada"
+	if scanner.Scan() {
 
-  case raca == "4":
-    raca = "Besta"
+		input := scanner.Text()
 
-  case raca == "5":
-    raca = "Vagante"
-  }
+		switch input {
+		case "1":
+			raca = "Humano"
+		case "2":
+			raca = "Elfo"
+		case "3":
+			raca = "Fada"
+		case "4":
+			raca = "Besta"
+		case "5":
+			raca = "Vagante"
+		}
+	}
 }
 
 func classes() {
-  fmt.Println("SELECIONE A SUA CLASSE:")
-  fmt.Print("|1| Guerreiro\n" +
-            "|2| Mago\n" +
-            "|3| Arqueiro\n" +
-            "|4| Noviço\n" +
-            "|5| Assassino\n")
-  fmt.Print("Qual classe deseja?: => ")
-  fmt.Scan(&classe)
+	fmt.Println("SELECIONE A SUA CLASSE:")
+	fmt.Print("|1| Guerreiro\n" +
+		"|2| Mago\n" +
+		"|3| Arqueiro\n" +
+		"|4| Noviço\n" +
+		"|5| Assassino\n")
+	fmt.Print("Qual classe deseja?: => ")
 
-  switch {
-  case classe == "1":
-    classe = "Guerreiro"
+	scanner := bufio.NewScanner(os.Stdin)
 
-  case classe == "2":
-    classe = "Mago"
+	if scanner.Scan() {
 
-  case classe == "3":
-    classe = "Arqueiro"
+		input := scanner.Text()
 
-  case classe == "4":
-    classe = "Noviço"
-
-  case classe == "5":
-    classe = "Assassino"
-  }
+		switch input {
+		case "1":
+			classe = "Guerreiro"
+		case "2":
+			classe = "Mago"
+		case "3":
+			classe = "Arqueiro"
+		case "4":
+			classe = "Noviço"
+		case "5":
+			classe = "Assassino"
+		}
+	}
 }
 
 func locais() {
-  fmt.Println("SELECIONE O LOCAL DE NASCIMENTO:")
-  fmt.Print("|1| Winterfell\n" +
-            "|2| Highgarden\n" + 
-            "|3| Carsterly Rock\n" +
-            "|4| Storm's End\n" +
-            "|5| Red Keep\n")
-  fmt.Print("Qual local deseja?: => ")
-  fmt.Scan(&local)
-  
-  switch {
-  case local == "1":
-    local = "Winterfell"
+	fmt.Println("SELECIONE O LOCAL DE NASCIMENTO:")
+	fmt.Print("|1| Winterfell\n" +
+		"|2| Highgarden\n" +
+		"|3| Carsterly Rock\n" +
+		"|4| Storm's End\n" +
+		"|5| Red Keep\n")
+	fmt.Print("Qual local deseja?: => ")
 
-  case local == "2":
-    local = "Highgarden"
+	scanner := bufio.NewScanner(os.Stdin)
 
-  case local == "3":
-    local = "Casterly Rock"
+	if scanner.Scan() {
 
-  case local == "4":
-    local = "Storm's End"
+		input := scanner.Text()
 
-  case local == "5":
-    local = "Red Keep"
-  }
+		switch input {
+		case "1":
+			local = "Winterfell"
+		case "2":
+			local = "Highgarden"
+		case "3":
+			local = "Casterly Rock"
+		case "4":
+			local = "Storm's End"
+		case "5":
+			local = "Red Keep"
+		}
+	}
 }
 
 func ficha() {
-  fmt.Printf("Olá, %s! Seja bem-vindo(a) a %s!\n", nome, local)
-  fmt.Printf("\nVeja as suas informações:\n-> Nome: %s\n-> Raça: %s\n-> Classe: %s\n-> Local: %s\n", nome, raca, classe, local)
+
+	fmt.Print("\nInsira o nome da sua personagem: => ")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		nome = scanner.Text()
+	}
+	fmt.Println("")
+	borda()
+	fmt.Printf("\nOlá, %s! Seja bem-vindo(a) a %s!\n", nome, local)
+	fmt.Printf("\nVeja as suas informações:\n-> Nome: %s\n-> Raça: %s\n-> Classe: %s\n-> Local: %s\n", nome, raca, classe, local)
 }
 
 func borda() {
-  linhas := ""
+	linhas := ""
 
-  for l := 0; l < 35; l++ {
-    linhas += "="
-  }
+	for l := 0; l < 35; l++ {
+		linhas += "="
+	}
 
-  fmt.Println(linhas)
+	fmt.Println(linhas)
 }
