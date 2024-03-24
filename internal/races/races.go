9c27323fbc1f41e4d618c43489d16207e6059e0b
Kage Jinsei <dadinho_123@outlook.com>
2024-03-24 09:15:16 -0300

    Adicionando 4 novas raças, sendo elas: Gigante, Anão, Orc e Dragonborn

--- internal/races/races.go
+++ internal/races/races.go
@@ -15,7 +15,11 @@ func Racas() {
 		"|2| Elfo\n" +
 		"|3| Fada\n" +
 		"|4| Besta\n" +
-		"|5| Vagante\n")
+		"|5| Vagante\n" +
+    "|6| Gigante\n" +
+    "|7| Anão\n" +
+    "|8| Orc\n" +
+    "|9| Dragonborn\n")
 
 	for {
 		fmt.Print("Qual raça deseja?: => ")
@@ -49,6 +53,26 @@ func Racas() {
 				return
 			case "5":
 				global.Raca = "Vagante"
+				border.Borda()
+				classes.Classes()
+				return
+      case "6":
+				global.Raca = "Gigante"
+				border.Borda()
+				classes.Classes()
+				return
+      case "7":
+				global.Raca = "Anão"
+				border.Borda()
+				classes.Classes()
+				return
+      case "8":
+				global.Raca = "Orc"
+				border.Borda()
+				classes.Classes()
+				return
+      case "9":
+				global.Raca = "Dragonborn"
 				border.Borda()
 				classes.Classes()
 				return
